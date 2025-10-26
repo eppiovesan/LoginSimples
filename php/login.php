85c1544c8fe60cc9b4d3c58f71a67b418685e1e8
Elize Piovesan <elizepiovesan.sci@gmail.com>
2025-10-26 16:13:25 -0300

    Organização de arquivos.

---
+++ php/login.php
@@ -0,0 +1,19 @@
+<?php
+
+
+//variaveis para conexão do banco de dados
+$host = "localhost";
+$port = "";
+$db_user = "";
+$db_password = "";
+$db_name = "";
+
+// a conexão será feita usando PDO
+try{
+ 
+ // sgdb;host;port;name
+ // usuario
+ // senha
+}catch (PDOException $erro){
+
+}
